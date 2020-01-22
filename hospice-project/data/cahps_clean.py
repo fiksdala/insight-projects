3c50f59d622c5b9cd2a6201e088bea9dc28e5847
Alexander Fiksdal <alexander.fiksdal@gmail.com>
2020-01-22 13:49:35 -0500

    finished cms pre-process

--- hospice-project/data/cahps_clean.py
+++ hospice-project/data/cahps_clean.py
@@ -4,19 +4,40 @@ import pandas as pd
 path = 'data/raw/cms/Hospice_Compare_-_Provider_CAHPS_Hospice_Survey_Data.csv'
 cahps_raw = pd.read_csv(path)
 
-# Keep ID, Measure Code, Score only
+# Keep ID (and rename cnn), Measure Code, Score only
 cahps_mcs = cahps_raw[['CMS Certification Number (CCN)',
                        'Measure Code', 'Score']].copy()
+cahps_mcs.rename(columns={'CMS Certification Number (CCN)': 'ccn'},
+                 inplace=True)
 
 # Reshape wide
-cahps_mcs_wide = cahps_mcs.pivot(index='CMS Certification Number (CCN)',
+cahps_mcs_wide = cahps_mcs.pivot(index='ccn',
                 columns='Measure Code',
                 values='Score')
 
-# Recode missing, make numeric
-cahps_mcs_wide = cahps_mcs_wide.replace(
-    'Not Available', 'nan').replace(
-    'Not  Available', 'nan').replace('Not Applicable','nan').astype(float)
+#%%
+cahps_mcs_wide.reset_index(inplace=True)
 
-# Save to csv
-cahps_mcs_wide.to_csv('data/interim/cms/cahps.csv')
+#%%
+# ccn to str
+cahps_mcs_wide['ccn'] = cahps_mcs_wide['ccn'].astype(str)
+
+#%%
+# Final adjustments
+mask = cahps_mcs_wide=='Not Available'
+cahps_mcs_wide[mask] = np.nan
+mask = cahps_mcs_wide=='Not  Available'
+cahps_mcs_wide[mask] = np.nan
+mask = cahps_mcs_wide=='Not Applicable'
+cahps_mcs_wide[mask] = np.nan
+
+#%%
+# Convert numeric and drop empty vars
+cahps_mcs_wide.drop(columns='EMO_REL_MBV')
+
+cahps_mcs_wide.iloc[:, 1:] = cahps_mcs_wide.iloc[:, 1:].astype(float)
+cahps_mcs_wide['ccn'] = cahps_mcs_wide['ccn'].astype(str)
+
+#%%
+# pickle
+pickle.dump(cahps_mcs_wide, open('data/interim/cahps.pickle', 'wb'))
---
+++ hospice-project/data/provider_general_clean.py
@@ -0,0 +1,36 @@
+#%%
+import pandas as pd
+import datetime as dt
+from dateutil import relativedelta
+
+#%%
+
+gen_raw = pd.read_csv('data/raw/cms/Hospice_-_General_Information.csv')
+
+# Keep ID, Ownership Type, and Certification Date
+own_cert = gen_raw[['CMS Certification Number (CCN)',
+                    'Ownership Type', 'Certification Date']].copy()
+
+# Rename ccn
+own_cert = own_cert.rename(columns={'CMS Certification Number (CCN)': 'ccn'})
+
+# ccn as string for merging later
+own_cert['ccn'] = own_cert['ccn'].astype(str)
+
+#%%
+# Make months_operation = months since certification to 1/1/17 (date of
+# cahps data start)
+cahps_start = dt.datetime.strptime('01/01/2017', '%m/%d/%Y')
+own_cert['days_operation'] = [
+    [cahps_start - dt.datetime.strptime(i, '%m/%d/%Y')][0].days
+    for i in own_cert['Certification Date']
+]
+
+#%%
+own_cert.head()
+
+#%%
+# pickle subset
+pickle.dump(own_cert[['ccn', 'Ownership Type', 'days_operation']],
+            open('provider_general.pickle', 'wb'))
+
